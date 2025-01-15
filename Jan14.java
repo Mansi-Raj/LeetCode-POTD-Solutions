ecb9847226f98ba44fb35c8ead64784d0400c31b
Mansi-Raj <mansi.raj.jnv@gmail.com>
2025-01-15 11:44:49 +0530

    add solution

---
+++ Jan14.java
@@ -0,0 +1,33 @@
+package POTD;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Jan14 {
+    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+        int n = A.length;
+        int[] C = new int[n];
+        C[n - 1] = n;
+        Set<Integer> set = new HashSet<>();
+        int count = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (A[i] == B[i]) {
+                count++;
+                C[i] = count;
+            } else {
+                if (set.contains(A[i]) && set.contains(B[i])) {
+                    count += 2;
+                    C[i] = count;
+                } else if (set.contains(A[i]) || set.contains(B[i])) {
+                    count++;
+                    C[i] = count;
+                } else {
+                    C[i] = count;
+                }
+            }
+            set.add(A[i]);
+            set.add(B[i]);
+        }
+        return C;
+    }
+}
